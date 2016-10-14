1ce384dadb06b765c7be6355359e58d1f6a53b96
Angelina Wang <angelinawang34@gmail.com>
2016-10-14 15:07:48 -0700

    wrote algorithm but doesn't work oops

--- main.py
+++ main.py
@@ -18,20 +18,60 @@ for key in l_pref.keys():
 
 def getLittle(name):
 	for little in little_list:
-		if little.getName == name:
+		if little.name == name:
 			return little
-	return NULL
+	return None
 
 def getBig(name):
 	for big in big_list:
-		if big.getName == name:
+		if big.name == name:
 			return big
-	return NULL
+	return None
+
+def keepGoing():
+	rtn = False
+	for little in little_list:
+		if not little.matched and len(little.preferences) > 0:
+			rtn = True
+	return rtn
 
 
 def pearings():
-	for big in big_list:
-		if (len(big.getPreferences()) > 0):
-			big.getPreferences()[0]
-	return 0
+	while keepGoing():
+		for little in little_list:
+			if (len(little.preferences) > 0):
+				big = getBig(little.preferences[0])
+				if big != None and not big.matched:
+					big.prospects += little
+					big.matched = True
+					little.matched = True
+					if big.prospects == 2:
+						one = big.prospects[0]
+						two = big.prospects[1]
+						if one not in big.preferences and two not in big.preferences:
+							if one.preferences.getIndex(big) > two.preferences.getIndex(big):
+								big.prospects = [two]
+								one.alterPreferences()
+							else:
+								big.prospects = [one]
+								two.alterPreferences()
+						elif one not in big.preferences:
+							big.prospects = [two]
+							one.alterPreferences()
+						elif two not in big.preferences:
+							big.prospects = [one]
+							two.alterPreferences()
+						else:
+							if big.preferences.getIndex(one) > big.preferences.getIndex(two):
+								big.prospects = [two]
+								one.alterPreferences()
+							else:
+								big.prospects = [one]
+								two.alterPreferences()
+	return None
+
+def returnPearings():
+	for little in little_list:
+		if little.matched:
+			print(little.name + " with " + little.preferences[0])
 
