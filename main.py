04a62e0592cf453995d08cf0b104b1150ecf806a
Nicole Peternel <nkpeternel@berkeley.edu>
2016-10-14 13:50:21 -0700

    success

--- main.py
+++ main.py
@@ -7,3 +7,32 @@ b_pref, l_pref = main(3)
 for keys, values in b_pref.items():
 	print('%s %s' % (keys, values)) # has a 'u' which is weird but stack overflow says it's unicode and it's all chill
 
+
+class Person(object):
+
+	def __init__(self, name):
+		self.name = name
+		self.matched = False
+
+	def isMatched(self):
+		return self.matched
+
+class Big(Person):
+
+	def __init__(self, name):
+		super(Big, self).__init__(name)
+		self.preferences = b_pref[self.name]
+		self.medium = 'big'
+
+
+class Little(Person):
+
+	def __init__(self, name):
+		super(Little, self).__init__(name)
+		self.preferences = l_pref[self.name]
+		self.medium = 'little'
+
+A = Big('A')
+print(A.name)
+print(A.isMatched())
+print(A.preferences)
